88e10b51d1c9bdeaa3bc688138a9b47a245a0d5a
Nathaniyell <essien.nathan@yahoo.com>
2023-10-07 19:06:04 +0100

    use switch to handle loading states

--- src/App.js
+++ src/App.js
@@ -4,32 +4,48 @@ import MoviesList from "./components/MovieList";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError]= useState(null)
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true); //once the function is instantiated, the loading state becomes true
-    setError(null)
-    try{
-      const response = await fetch("https://swapi.dev/api/film");
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      } //check that the response is okay before even passing it to be converted to json
       const data = await response.json();
-  if(!response.ok){
-    throw new Error("Something went wrong")
-  }
       const transformedMovies = data.results.map((movie) => {
         return {
           id: movie.episode_id,
           title: movie.title,
-          openingText: movie.opening_craw,
+          openingText: movie.opening_crawl,
           releaseDate: movie.release_date,
         };
       });
       setMovies(transformedMovies);
       setIsLoading(false); //once the movies have been loaded, the loading is set back to false
-    }catch(err){
-      setError(err.message)
+    } catch (err) {
+      setError(err.message);
     }
-    setIsLoading(false) //once the request is successful or there is an error while sending the request
-    
+    setIsLoading(false); //once the request is successful or there is an error while sending the request
+  }
+
+  let content = <p>Found no movies</p>;
+
+  switch (content) {
+    case isLoading:
+      content = <p>Loading...</p>;
+      break;
+    case error:
+      content = <p>{error}</p>;
+      break;
+    case movies.length > 0:
+      content = <MoviesList movies={movies} />;
+      break;
+    default:
+      content = <p>Found no movies</p>;
+      break;
   }
 
   return (
@@ -38,10 +54,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p> }
+        {content}
       </section>
     </>
   );
