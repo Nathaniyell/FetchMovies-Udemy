e2523f8d6bfc84c46d0b7a08239d952ff6654cec
Nathaniyell <essien.nathan@yahoo.com>
2023-10-07 20:20:06 +0100

    add new firebase api for sending post request

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,11 @@ function App() {
   const [error, setError] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
+    setIsLoading(true); //once the function is instantiated, the loading state becomes true
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      // const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://fetchmovies-udemy-default-rtdb.firebaseio.com/movies.json'); //`/movies.json` creates a new node on the url which can be used to send post requests or patch in firebase.
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -32,8 +33,10 @@ function App() {
     } catch (error) {
       setError(error.message);
     }
-    setIsLoading(false);
-  }, []);
+    setIsLoading(false); //once the request is successful or there is an error while sending the request
+
+  }, []); //An empty dependency array on the useEffect hook means that the effect will run only once when the component mounts. When there is no second argument at all, the effect is ran everytime there is a change on the component. When the dependency array has values, the effect will only run when those values changes
+
 
   useEffect(() => {
     fetchMoviesHandler();
@@ -58,7 +61,7 @@ function App() {
   }
 
   return (
-    <React.Fragment>
+    <>
       <section>
         <AddMovie onAddMovie={addMovieHandler} />
       </section>
@@ -66,8 +69,16 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
-    </React.Fragment>
+    </>
   );
 }
 
 export default App;
+/*
+fetch("", {}) the second argument allows us to pass in a javascript object to specify other options e.g extra headers,body, or change the http request method. In most cases you won't need the second argument because by default a get request is sent so you might not necessarily need to specifiy it
+-When using async aawait, you used try and catch to check for errors. But when just using the fetch api without async await, you chain .then blocks and then also use .catch() to cath potential errors
+
+useCallback hook in React is used to optimize your React application's performance by memoizing (remembering) a function.
+UseCallback hook is used specifically for performance optimization. It is used to wrap around functions to prevent the function from rerendering too many times and also to rerender when a value which was specified in the dependency array changes.
+Memoization: It's like putting a bookmark on a page. When you wrap a function with useCallback, React will remember the function as long as its dependencies remain the same. If they change, React will provide you with a new (updated) function.
+*/
